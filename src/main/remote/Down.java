bb9c4f3287626db9fb39d7059aa45fb84771c6cb
Jesus <sisniega.serrano@gmail.com>
2026-04-28 21:54:16 -0400

    now functions on light.

---
+++ src/main/remote/Down.java
@@ -0,0 +1,87 @@
+package remote;
+import vendors.*;
+
+/**
+ * A child class of action that decreases the volume of a tv.
+ * 
+ * @author Willow Sapphire
+ * @version Fall 2022
+ */
+public class Down extends Action
+{
+    /**
+     * The tv for which to change the volume.
+     */
+    private Television tv;
+
+    private SmartLight light; 
+
+    /**
+     * Creates this action object.
+     * 
+     * @param tv the tv to connect to
+     */
+    public Down(Television tv)
+    {
+        super("Volume Down");
+        this.tv = tv;
+    }
+
+    public Down(SmartLight light)
+    {
+        super("Brightness Down");
+        this.light = light;
+    }
+
+    public Down(Television tv, SmartLight light)
+    {
+        super("Volume/Brightness Down");
+        this.tv = tv;
+        this.light = light;
+    }
+
+    /**
+     * Turns the volume of the tv down by one.
+     * 
+     * @return false if the tv is not on yet or the volume is at zero, true otherwise
+     */
+    @Override
+    public boolean performAction()
+    {
+        boolean didntFail = true;
+        
+        if (tv != null) {
+            if (!tv.isOn())
+            {
+                System.out.println("You need to turn on the tv before you can change the volume.");
+                didntFail = false;
+            }
+            else if (tv.getVolume() == 0)
+            {
+                System.out.println("Volume already at zero");
+                didntFail = false;
+            }
+            else
+            {
+                tv.decreaseVolume();
+                System.out.printf("Volume down. Now at %d\n", tv.getVolume());
+            }
+        }
+        
+        if (light != null) {
+            if (!light.isOn()) {
+                System.out.println("You need to turn on the light before you can change the brightness.");
+                didntFail = false;
+            else if (light.getIntensity() == 0) {
+                System.out.println("Brightness already at zero");
+                didntFail = false;
+            }
+            else {
+                light.decreaseIntensity();
+                System.out.printf("Brightness down. Now at %d\%\n", light.getIntensity());
+            }
+        }
+        
+        return didntFail;
+    }
+}
