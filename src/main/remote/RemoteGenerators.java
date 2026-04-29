5c2620da74bfb3e0ef8c5b461aa8c526c5e9dcba
Jesus <sisniega.serrano@gmail.com>
2026-04-28 21:36:21 -0400

    Create lightRemote and superRemote. Refactor tvRemote.

--- src/main/remote/RemoteGenerators.java
+++ src/main/remote/RemoteGenerators.java
@@ -22,10 +22,38 @@ public class RemoteGenerators
         Remote tvRemote = new Remote(8);
         tvRemote.setButton(new Power(tv), 0);
         tvRemote.setButton(new ChannelListing(tv), 1);
-        tvRemote.setButton(new VolumeUp(tv), 2);
-        tvRemote.setButton(new VolumeDown(tv), 3);
-        tvRemote.setButton(new ChannelForward(tv), 4);
-        tvRemote.setButton(new ChannelBackward(tv), 5);
+        tvRemote.setButton(new Up(tv), 2);
+        tvRemote.setButton(new Down(tv), 3);
+        tvRemote.setButton(new Forward(tv), 4);
+        tvRemote.setButton(new Backward(tv), 5);
+        tvRemote.setButton(new Subscribe(tv, userInput), 6);
+        tvRemote.setButton(new Unsubscribe(tv, userInput), 7);
+        return tvRemote;
+    }
+
+    public static Remote lightRemote(SmartLight light)
+    {
+        Remote tvRemote = new Remote(5);
+        tvRemote.setButton(new Power(light), 0);
+        tvRemote.setButton(new Up(light), 1);
+        tvRemote.setButton(new Down(light), 2);
+        tvRemote.setButton(new Forward(light), 3);
+        tvRemote.setButton(new Backward(light), 4);
+        return tvRemote;
+    }
+
+    /*
+    * Combines tvRemote and lightRemote into one, for least amount of buttons, simulating a real remote.
+    */
+    public static Remote superRemote(Television tv, Scanner userInput, SmartLight light)
+    {
+        Remote tvRemote = new Remote(8);
+        tvRemote.setButton(new Power(tv, light), 0);
+        tvRemote.setButton(new ChannelListing(tv), 1);
+        tvRemote.setButton(new Up(tv, light), 2);
+        tvRemote.setButton(new Down(tv, light), 3);
+        tvRemote.setButton(new Forward(tv, light), 4);
+        tvRemote.setButton(new Backward(tv, light), 5);
         tvRemote.setButton(new Subscribe(tv, userInput), 6);
         tvRemote.setButton(new Unsubscribe(tv, userInput), 7);
         return tvRemote;
